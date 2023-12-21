0aaaa7df28d5768f511732a8a78af5100b0bc63c
arturo785 <joarceus@hotmail.com>
2023-12-21 17:42:47 -0600

    addition of my impl

---
+++ app/src/test/java/com/plcoding/testingcourse/part4/presentation/BadProfileViewModelTest.kt
@@ -0,0 +1,24 @@
+package com.plcoding.testingcourse.part4.presentation
+
+import android.app.Application
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class BadProfileViewModelTest {
+
+    private lateinit var viewModel: BadProfileViewModel
+
+    @BeforeEach
+    fun setUp() {
+        viewModel = BadProfileViewModel(Application()) // <-- This is the problem, Application is passed in just to make the test pass
+    }
+
+    @Test
+    fun `Test saving profile`() {
+
+    }
+
+}
---
+++ app/src/test/java/com/plcoding/testingcourse/part4/presentation/GoodProfileViewModelTest.kt
@@ -0,0 +1,24 @@
+package com.plcoding.testingcourse.part4.presentation
+
+import com.plcoding.testingcourse.part4.domain.AnalyticsLogger
+import com.plcoding.testingcourse.part4.domain.LogParam
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+
+internal class GoodProfileViewModelTest {
+
+    private lateinit var viewModel: GoodProfileViewModel
+
+
+    /*
+    * This one allows for using fake implementations of the interface 
+    * */
+    @BeforeEach
+    fun setUp() {
+        viewModel = GoodProfileViewModel(
+            analytics = object : AnalyticsLogger {
+                override fun logEvent(key: String, vararg params: LogParam<Any>) = Unit
+            }
+        )
+    }
+}
